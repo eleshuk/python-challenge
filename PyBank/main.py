82763677a802891e44d0b374e359ace21b7151d3
eleshuk <49963868+eleshuk@users.noreply.github.com>
2020-10-30 18:46:08 -0700

    began working on calculating average profits in pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,12 @@ csvpath = os.path.join('C:\\Users\\19143\\Desktop\\DataClass\\python-challenge\\
 month_count = 0
 total = 0
 
+# set max and min
+max_profit = 999999999
+max_month = ""
+min_profit = -999999999
+min_month = ""
+
 with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
@@ -21,24 +27,25 @@ with open(csvpath, "r") as csvfile:
     print(f"CSV Header: {csv_header}")
 
     # read each row of the data after the header and set profits, total, and month_count
+    # calculate the total number of months included in the dataset   
     for row in csvreader:
         profit = int(row[1])
         month = row[0]
+        # calculate the net profits over the entire period
         total= total + profit
         # add another month
         month_count += 1
-    
+   
 print("Financial Records")
 print("-----------------------------")
+# print the total number of months included in the dataset 
 print(f'Total Months: {month_count}')
+# print the net total amount of "profits/losses" over the entire period
 print(f'Net Total: ${total}')
 
-# calculate the total number of months included in the dataset
-
-# calculate the net total amount of "profits/losses" over the entire period
-
 # calculate the average of the changes in "profits/losses over the entire period"
-
+average_profit = profit / month_count 
+print(average_profit)
 # calculate the greatest increase in profits (date and amount) over the entire period
 
 # calculate the greatest decrease in losses (date and amount) over the entire period
