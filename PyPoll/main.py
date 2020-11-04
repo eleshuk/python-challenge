5a1821b2ce03e8374bc682b3a1147ea11896f332
eleshuk <49963868+eleshuk@users.noreply.github.com>
2020-11-03 22:10:58 -0800

    added final changes to pypoll code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,67 +1,10 @@
-# # import dependencies
-# import os
-# import csv
-# import sys
-
-# # set your variables
-# votes = []
-# vote_count = 0
-# last_count = 0
-# candidate_list = []
-# candidate_count = 0
-# candidate_percentage = 0
-
-# # set csvpath
-# csvpath = os.path.join ('Resources','election_data.csv')
-
-
-# with open(csvpath, "r") as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=',')
-
-#     # read the header row first (skip this if there is no header)
-#     csv_header = next(csvreader)
-
-#     for row in csvreader:
-#         votes.append(row[2])
-
-#     total_votes = len(votes)
-
-#     for name in candidate_list:
-#         if name not in candidate_list:
-#             x = name
-#             votes.append(name)     
-
-# candidate = candidate_list[0]
-
-
-# for candidate in candidate_list:
-#     for vote in votes:
-#         if candidate == vote:
-#             vote_count += 1
-    
-#     if last_count < count:
-#         win = candidate
-
-#     count = 0 
-#     last_count = 0
-        
-
-# print("Election Results")
-# print('-------------------------')
-# print(f"Total Votes: {total_votes}")
-# print('-------------------------')
-# print(f"Winner: {win}")
-# print('-------------------------')
-
 
 # import dependencies
 import os
 import csv
 import sys
 
-# these are the important variables for the election result
 votes = []
-# the following lists are 1- unique candidates, 2- percentage of their votes, 3- total count of their votes
 candidates = []
 percent_vote = []
 vote_count = []
@@ -78,7 +21,6 @@ with open(csvpath, "r") as csvfile:
         
 total_votes = len(votes)
 
-# find all the candidates and assign their indexes
 for name in candidates:
    if name not in candidates:
         candidates.append(name)
@@ -90,15 +32,15 @@ last_count = 0
 
 for candidate in candidates:
     for vote in votes:
-        if candidate == votes:
+        if candidate == vote:
             total_votes += 1
     percent_vote = total_votes / len(votes)
-    percent_vote.append(percent)
-    vote_count.append(count)
+    percent_vote.append(percent_vote)
+    vote_count.append(vote_count)
 
-        if last_count < total_votes:
-            winner = candidate    
-        print(f"{candidate}: {percent_vote:.3%} ({vote_count})")
+    if last_count < total_votes:
+        Winner = candidates
+    print(f"{candidate}: {percent_vote:.3%} ({vote_count})")
 
     # reset vote count to zero
     last_count = total_votes
@@ -108,5 +50,9 @@ print("Election Results")
 print('-------------------------')
 print(f"Total Votes: {total_votes}")
 print('-------------------------')
+# for candidate in candidates:
+#     index = candidates.index(candidate)
+# print(f"{candidates}: {percent_vote:.3%} ({vote_count})")
 print(f"Winner: {Winner}")
 print('-------------------------')
+
