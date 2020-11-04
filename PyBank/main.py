3290dd77a8ec00c5163cbf5f5c10d6d17ef89691
eleshuk <49963868+eleshuk@users.noreply.github.com>
2020-11-03 19:24:21 -0800

    finished PyBank, added .txt analysis file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 # import the os module
 import os
-
+import sys
 # module for reading the csv file
 import csv
 csvpath = os.path.join('C:\\Users\\19143\\Desktop\\DataClass\\python-challenge\\PyBank\\Resources\\budget_data.csv')
@@ -41,28 +41,26 @@ with open(csvpath, "r") as csvfile:
         if month_count ==1:
             last_profit = profit
         else:
-            if (int(row[1]) - last_profit) > max_profit:
-                max_profit = int(row[1])- last_profit
-                max_month = row[0]
-            if (int(row[1]) - last_profit) < min_profit:
-                min_profit = int(row[1]) - last_profit
-                min_month = row[0]
-            sum_change += int(row[1]) - last_profit
-            total_profit += int(row[1])
-            last_profit = int(row[1])
-    
+            # if profit - last profit
+            if (profit - last_profit) > max_profit:
+                # maximum profit = profit - last profit
+                max_profit = profit- last_profit
+                # maximum month = month
+                max_month = month
+            # if profit - profits is less than the min_profit 
+            if (profit - last_profit) < min_profit:
+                # minimum profit is equal to the profit - the last profit
+                min_profit = profit - last_profit
+                # min month = month 
+                min_month = month
+            sum_change += profit - last_profit
+            total_profit += profit
+            last_profit = profit
+        # update total
         total= profit + total
+    # calculate average change
     average_change = sum_change/(month_count-1)
-    # average_change = round(average_change, 2)
-    
 
-            # calculate the change
-            #profit_change = profit - last_profit
-            # update last_profit
-            #last_profit = profit_change / month_count
-        # calculate the net profits over the entire period
-    #total= profit + total
-        # add another month
 
 print("Financial Records")
 print("-----------------------------")
@@ -70,14 +68,23 @@ print("-----------------------------")
 print(f'Total Months: {month_count}')
 # print the net total amount of "profits/losses" over the entire period
 print(f'Total: ${total:0,.0f}')
+print(f'Average Change: ${average_change:0,.2f}')
+print(f'Greatest Increase in Profits: {max_month}, (${max_profit})')
+print(f'Greatest Decrease in Profits: {min_month}, (${min_profit})')
 
-print(f'Total Change: ${average_change:0,.2f}')
+stdoutOrigin=sys.stdout 
+text_path = os.path.join('Analysis', 'PyBank_analysis.txt')
+sys.stdout = open(text_path, "w")
 
-# # calculate the average of the changes in "profits/losses over the entire period"
+print("Financial Records")
+print("-----------------------------")
+# print the total number of months included in the dataset 
+print(f'Total Months: {month_count}')
+# print the net total amount of "profits/losses" over the entire period
+print(f'Total: ${total:0,.0f}')
+print(f'Average Change: ${average_change:0,.2f}')
+print(f'Greatest Increase in Profits: {max_month}, (${max_profit})')
+print(f'Greatest Decrease in Profits: {min_month}, (${min_profit})')
 
-# # calculate the greatest increase in profits (date and amount) over the entire period
-# # greatest_increase = 
-# # calculate the greatest decrease in losses (date and amount) over the entire period
-# # greatest_decrease = 
- 
-
+sys.stdout.close()
+sys.stdout=stdoutOrigin
